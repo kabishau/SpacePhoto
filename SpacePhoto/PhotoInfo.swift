94040b404359ce204d8b72149881c9f3a367e6c5
user131656 <user131656@ny218.local>
2018-02-21 16:59:58 -0500

    PhotoInfo object has been added

---
+++ SpacePhoto/PhotoInfo.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+
+struct PhotoInfo: Codable {
+    var title: String
+    var description: String
+    var url: URL
+    var copyright: String?
+    
+    enum Keys: String, CodingKey {
+        case title
+        case description = "explanation"
+        case url
+        case copyright
+    }
+    
+    init(from decoder: Decoder) throws {
+        let valueContainer = try decoder.container(keyedBy: Keys.self)
+        self.title = try valueContainer.decode(String.self, forKey: Keys.title)
+        
+    }
+}
