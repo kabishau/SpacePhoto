4b40acf08a59b3054437b49fdc45414df2560194
user131656 <user131656@ny218.local>
2018-02-24 14:18:46 -0500

    Displaying Title, Description and Copyright from API

---
+++ SpacePhoto/PhotoInfoController.swift
@@ -0,0 +1,21 @@
+import Foundation
+
+
+struct PhotoInfoController {
+    func fetchPhotoInfo(completion: @escaping (PhotoInfo?) -> Void) {
+        let baseURL = URL(string: "https://api.nasa.gov/planetary/apod")!
+        let query: [String: String] = ["api_key": "DEMO_KEY", "date": "2012-10-25"]
+        let url = baseURL.withQueries(query)!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            let jsonDecoder = JSONDecoder()
+            if let data = data, let photoInfo = try? jsonDecoder.decode(PhotoInfo.self, from: data) {
+                print("works")
+                completion(photoInfo)
+            } else {
+                print("Either no data was returned, or data was not serialized.")
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
+}
--- SpacePhoto/ViewController.swift
+++ SpacePhoto/ViewController.swift
@@ -7,13 +7,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var copyrightLabel: UILabel!
     
+    let photoInfoController = PhotoInfoController()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        descriptionLabel.text = ""
+        copyrightLabel.text = ""
+        
+        photoInfoController.fetchPhotoInfo { (photoInfo) in
+            self.title = photoInfo?.title
+            self.descriptionLabel.text = photoInfo?.description
+            if let copyright = photoInfo?.copyright {
+                self.copyrightLabel.text = "Copyright \(copyright)"
+            } else {
+                self.copyrightLabel.isHidden = true
+            }
+        }
     }
-
-
-
 }
 
